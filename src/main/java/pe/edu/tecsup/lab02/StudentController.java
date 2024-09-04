ab47b95ad0100f8fbbd10f72c9b8c034baa6162b
Alonso-S <alonso.s2k5@gmail.com>
2024-09-03 20:21:29 -0500

    Modificación de StudentController

--- src/main/java/pe/edu/tecsup/lab02/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab02/StudentController.java
@@ -5,5 +5,10 @@ public class StudentController {
 
     public void controller(){
         //TO DO
+
+    }
+    public void exampleMethod() {
+        // Método de ejemplo
+        System.out.println("Este es un método de ejemplo");
     }
 }
